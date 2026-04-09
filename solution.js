429a21c5ced6fb5fbd5a35573922f865448d3fb2
Nelly Belikova <nelly.belikova@gmail.com>
2026-04-09 17:54:22 +0300

    Update solution.js

--- solution.js
+++ solution.js
@@ -4,6 +4,17 @@
  * @return {number[]}
  */
 
+//решение двумя указателями
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) return [i, j];
+        }
+    }
+    return [];
+};
+
+//решение с indexOf
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         let search = target - nums[i];
